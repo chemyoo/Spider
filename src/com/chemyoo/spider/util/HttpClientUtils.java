76aadab43800d13d139474ae6f2869070c8ea88a
chemyoo <chemyoo@foxmail.com>
2019-02-25 09:26:37 +0800

    添加字符集编码
    Signed-off-by: chemyoo <chemyoo@foxmail.com>

--- src/com/chemyoo/spider/util/HttpClientUtils.java
+++ src/com/chemyoo/spider/util/HttpClientUtils.java
@@ -1,5 +1,6 @@
 package com.chemyoo.spider.util;
 
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -18,9 +19,48 @@ public class HttpClientUtils {
 
 	private static Logger logger = Logger.getLogger(HttpClientUtils.class);
 	
+	private static String charset = CharSets.UTF_8.getCharset();
+	
 	protected static Random random = new Random();
 
 	private HttpClientUtils() {
+	}
+	
+	
+	public enum CharSets {
+		/**
+		 * 字符集编码：
+		 * UTF-8
+		 */
+		UTF_8("utf-8"),
+		/**
+		 * 字符集编码：
+		 * GBK
+		 */
+		GBK("gbk"),
+		/**
+		 * 字符集编码：
+		 * ISO-8859-1
+		 */
+		ISO_8859_1("iso-8859-1"),
+		/**
+		 * 字符集编码：
+		 * GB2312
+		 */
+		GB2312("gb2312");
+		
+		private String charset;
+		
+		CharSets(String charset){
+			this.charset = charset;
+		}
+		
+		/**
+		 * @return 字符集
+		 */
+		public String getCharset() {
+			return this.charset;
+		}
 	}
 	
 	public static String post(String text,String link) {
@@ -33,11 +73,11 @@ public class HttpClientUtils {
             conn.setDoInput(true);
             conn.setUseCaches(false);
             conn.setRequestProperty("Connection", "Keep-Alive");//保持长链接
-            conn.setRequestProperty("Charset", "UTF-8");
+            conn.setRequestProperty("Charset", charset);
             // 设置文件类型:
             conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
             // 设置接收类型否则返回415错误
-            // conn.setRequestProperty("accept","*/*")此处为设置接受所有类型，以此来防范返回415;
+            // conn.setRequestProperty("accept","*/*")，此处为设置接受所有类型，以此来防范返回415
             conn.setRequestProperty("accept","application/json");
             
 			// 设置连接主机超时（单位：毫秒）  
@@ -59,7 +99,13 @@ public class HttpClientUtils {
                 outwritestream.close();
             }
             if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                result = IOUtils.toString(conn.getInputStream(), "UTF-8");
+                result = IOUtils.toString(conn.getInputStream(), charset);
+            } else {
+            	InputStream inputStream = conn.getErrorStream();
+            	if(inputStream == null) {
+            		inputStream = conn.getInputStream();
+            	}
+            	logger.error("connect failure: " + IOUtils.toString(inputStream, charset));
             }
             conn.disconnect();
         } catch (Exception e) {
