72b80ef787610126a1c1ff5ae1ff9f12b76b62f1
chemyoo <chemyoo@Prd-liujianqing>
2019-11-05 16:48:00 +0800

    删除不需要的组件

--- src/com/chemyoo/spider/core/ImagesUtils.java
+++ src/com/chemyoo/spider/core/ImagesUtils.java
@@ -1,26 +1,15 @@
 package com.chemyoo.spider.core;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import com.chemyoo.spider.util.PropertiesUtil;
 
@@ -35,95 +24,10 @@ public class ImagesUtils {
 	
 	private static final Logger LOG = Logger.getLogger(ImagesUtils.class);
 	
-	// 编码
-	private static final String ECODING = "UTF-8";
-	// 获取img标签正则
-	private static final String IMGURL_REG = "<img.*src=(.*?)[^>]*?>";
-	// 获取src路径的正则
-	private static final String IMGSRC_REG = "http:\"?(.*?)(\"|>|\\s+)";
-
 	public static void downloadPic(String dir, String referer) {
-		// 获得html文本内容
 		download(dir, referer);
-					
 	}
 
-	/***
-	 * 获取HTML内容
-	 * 
-	 * @param url
-	 * @return
-	 * @throws IOException 
-	 * @throws Exception
-	 */
-	public static String getHTML(String url) throws IOException {
-		URL uri = new URL(url);
-		URLConnection connection = uri.openConnection();
-		InputStream in = connection.getInputStream();
-		byte[] buf = new byte[1024];
-		StringBuilder sb = new StringBuilder();
-		while (in.read(buf, 0, buf.length) > 0) {
-			sb.append(new String(buf, ECODING));
-		}
-		in.close();
-		return sb.toString();
-	}
-
-	/***
-	 * 获取ImageUrl地址
-	 * 
-	 * @param html
-	 * @return
-	 */
-	private static List<String> getImageUrl(String html) {
-		Matcher matcher = Pattern.compile(IMGURL_REG).matcher(html);
-		List<String> listImgUrl = new ArrayList<>();
-		String matcherHtml;
-		boolean isAdd;
-		while (matcher.find()) {
-			isAdd = true;
-			matcherHtml = matcher.group();
-			Document doc = Jsoup.parse(matcherHtml); 
-			Elements elements = doc.getAllElements();
-			Iterator<Element> iterator = elements.iterator();
-			Element element;
-			String width;
-			String height;
-			while(iterator.hasNext()) {
-				element = iterator.next();
-				width = element.attr("width");
-				height = element.attr("height");
-				if(LinkQueue.isNotBlank(width , height)) {
-					if(Integer.parseInt(width.trim().replace("px", "")) <100 
-							|| Integer.parseInt(height.trim().replace("px", "")) <100) {
-						isAdd = false;
-						break;
-					}
-				}
-			}
-			if(isAdd)
-				listImgUrl.add(matcherHtml);
-		}
-		return listImgUrl;
-	}
-
-	/***
-	 * 获取ImageSrc地址
-	 * 
-	 * @param listImageUrl
-	 * @return
-	 */
-	private static List<String> getImageSrc(List<String> listImageUrl) {
-		List<String> listImgSrc = new ArrayList<>();
-		for (String image : listImageUrl) {
-			Matcher matcher = Pattern.compile(IMGSRC_REG).matcher(image);
-			while (matcher.find()) {
-				listImgSrc.add(matcher.group().substring(0,
-						matcher.group().length() - 1));
-			}
-		}
-		return listImgSrc;
-	}
 
 	/***
 	 * 下载图片
