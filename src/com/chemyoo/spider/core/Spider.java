2d81cba3672df2cc0864412130a7e70b0adecdf8
chemyoo <chemyoo@foxmail.com>
2019-11-06 20:45:28 +0800

    repair a bug

--- src/com/chemyoo/spider/core/Spider.java
+++ src/com/chemyoo/spider/core/Spider.java
@@ -248,7 +248,8 @@ public class Spider {
 						if(isNotAdd)
 							continue label;
 					}
-				} else if(href.startsWith(this.referer)) {
+				} 
+				if(href.startsWith(this.referer)) {
 					// 只收集本网站地址
 					LinkQueue.addPageUrl(href);
 				}
